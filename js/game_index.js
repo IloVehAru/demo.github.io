c877f511adc821fe57c133010c8d900d82567dae
IloVehAru <895387279@qq.com>
2017-05-26 18:40:39 +0800

    2

--- js/game_index.js
+++ js/game_index.js
@@ -56,7 +56,7 @@ function initScence1(){  //初始化第一界面
 	for(var j=0;j<5;j++){
 		for(var i=0;i<5;i++){
 			var k=Math.floor(Math.random()*5);
-			title[j][i]=new createjs.Text(title_str[j].charAt(i),"bold 160px FANTASY","#FFFFFF");
+			title[j][i]=new createjs.Text(title_str[j].charAt(i),"bold 120px IPPACT blackoak-std","#FFFFFF");
 			shadow.graphics.beginFill(color_shadow[k]).drawRoundRect(120+200*i,220+220*j,160,160,20);
 			if(title[j][i].text==" "){
 				icon.graphics.beginFill(color[k]).drawRoundRect(120+200*i,210+220*j,160,160,20);
@@ -67,7 +67,7 @@ function initScence1(){  //初始化第一界面
 			
 			title[j][i].regX=title[j][i].getBounds().width/2;
 			title[j][i].x=200+200*i;
-			title[j][i].y=175+220*j;
+			title[j][i].y=185+220*j;
 			scence1.addChild(title[j][i]);
 		}
 	}
@@ -166,10 +166,10 @@ function initLevel(level_count){
 	var shadow=new createjs.Shape();
 	shadow.graphics.beginFill("#cecece").drawRoundRect(120,80,960,100,20);
 	level.addChild(shadow);
-	var score_title=new createjs.Text("","bold 80px FANTASY","#FFFFFF");
-	var round_title=new createjs.Text("","bold 80px FANTASY","#FFFFFF");
-	round_title.x=700;round_title.y=80;
-	score_title.x=200;score_title.y=80;
+	var score_title=new createjs.Text("","bold 60px  blackoak-std","#FFFFFF");
+	var round_title=new createjs.Text("","bold 60px  blackoak-std","#FFFFFF");
+	round_title.x=700;round_title.y=90;
+	score_title.x=200;score_title.y=90;
 	
 	level.addChild(round_title);
 	level.addChild(score_title);
@@ -239,7 +239,7 @@ function initLevel(level_count){
 	function gameover(){
 		gameend=true;
 		var over=new createjs.Shape();
-		var final_score=new createjs.Text(score,"bold 200px FANTASY","#ff4040");
+		var final_score=new createjs.Text(score,"bold 200px  blackoak-std","#ff4040");
 		var final_button=new createjs.Shape();
 		level.addChild(over);
 		level.addChild(final_score);
